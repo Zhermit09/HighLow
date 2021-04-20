6ae4e012b8b6cb78e5e9be9aad7846840d484365
Zhermit09 <71440273+Zhermit09@users.noreply.github.com>
2021-04-20 21:36:42 +0200

    Card Deck Done

---
+++ HighLow/Cards.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighLow
+{
+    class Cards
+    {
+        private string symbol;
+        private ConsoleColor color;
+        private CardsType type;
+        private CardsName name;
+
+        public Cards(CardsName name, CardsType type, string symbol, ConsoleColor color)
+        {
+            this.name = name;
+            this.type = type;
+            this.symbol = symbol;
+            this.color = color;
+        }
+
+        public void Deal()
+        {
+            Console.WriteLine((int)name + " " + name + " " + type + "" + symbol);
+        }
+
+        public ConsoleColor GetColor() { return color; }
+        public CardsType Type() { return type; }
+        public CardsName Name() { return name; }
+    }
+
+    public enum CardsType
+    {
+        CLUBS, DIAMONDS, HEARTS, SPADES, ERROR
+    }
+    public enum CardsName
+    {
+        NONE = 0,
+        TWO = 2,
+        THREE = 3,
+        FOUR = 4,
+        FIVE = 5,
+        SIX = 6,
+        SEVEN = 7,
+        EIGHT = 8,
+        NINE = 9,
+        TEN = 10,
+        JACK = 11,
+        QUEEN = 12,
+        KING = 13,
+        ACE = 14
+    }
+}
--- HighLow/Program.cs
+++ HighLow/Program.cs
@@ -8,8 +8,58 @@ namespace HighLow
 {
     class Program
     {
+        List<Cards> Deck = new List<Cards>();
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try { Console.SetWindowSize(237, 63); }
+            catch { Console.SetWindowSize(200, 50); }
+            Program program = new Program();
+            program.CreateDeck();
+        }
+        void CreateDeck()
+        {
+            CardsType typo;
+            CardsName name;
+            string[] symbol = new string[] { "♣ ", "♦", "♥", "♠", " " };
+            ConsoleColor[] color = new ConsoleColor[] { ConsoleColor.Cyan, ConsoleColor.DarkRed, ConsoleColor.Magenta, ConsoleColor.Green, ConsoleColor.DarkYellow };
+            int i = 0;
+
+            foreach (string type in Enum.GetNames(typeof(CardsType)))
+            {
+                typo = (CardsType)Enum.Parse(typeof(CardsType), type);
+
+                if (typo != CardsType.ERROR)
+                {
+                    foreach (string nemo in Enum.GetNames(typeof(CardsName)))
+                    {
+                        name = (CardsName)Enum.Parse(typeof(CardsName), nemo);
+                        if (name != CardsName.NONE)
+                        {
+                            Deck.Add(new Cards(name, typo, symbol[i], color[i]));
+                        }
+                    }
+                }
+                i++;
+            }
+            start();
+        }
+
+        void start()
+        {
+            
+            foreach (Cards card in Deck)
+            {
+                Console.ForegroundColor = card.GetColor();
+                card.Deal();
+            }
+            Cards Error = new Cards(CardsName.NONE, CardsType.ERROR, "/!\\", ConsoleColor.DarkYellow);
+            Console.ForegroundColor = Error.GetColor();
+            Error.Deal();
+            Console.WriteLine(Deck.Count());
+
+            Console.ReadLine();
         }
     }
 }
